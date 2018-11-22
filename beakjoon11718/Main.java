91f776ef575bcbd0a0bafa482c8fd9c591f12244
poemtree <yym1232@gmail.com>
2018-11-22 18:39:50 +0900

    백준 11718번 문제 입력 받기

---
+++ beakjoon11718/Main.java
@@ -0,0 +1,15 @@
+package beakjoon11718;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	       sc.hasNextLine();
+	      while(sc.hasNext()) {
+	         System.out.println(sc.nextLine());
+	      }
+	   }
+
+}
